e0b0b5f3ed4a30a0f924fd85087dc57cdb95c747
Juan Tenezaca <tenj34@gmail.com>
2018-10-20 20:38:37 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -11,7 +11,8 @@ class App < Sinatra::Base
   end
 
   post '/checkout' do
-  session['item'] = params[:item]
-  @session = session
-  erb :checkout
+    session['item'] = params[:item]
+    @session = session
+    erb :checkout
+  end
 end
